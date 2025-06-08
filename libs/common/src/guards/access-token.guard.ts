d93c7880245a87be7f269fba1f4806bf8fb803c5
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-08 16:33:35 +0700

    fix error messga

--- libs/common/src/guards/access-token.guard.ts
+++ libs/common/src/guards/access-token.guard.ts
@@ -57,7 +57,7 @@ export class AccessTokenGuard implements CanActivate {
                     }
                 }
             }
-        }).catch(() => { throw new ForbiddenExceptionRpc() })
+        }).catch(() => { throw ForbiddenExceptionRpc() })
         console.log(rolesWithPerms);
 
         const allPerms = rolesWithPerms.flatMap(r => r.permissions)
