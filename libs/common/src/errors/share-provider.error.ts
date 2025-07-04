6bef6b0dcc62ce1b55bb906a73802b7011f64155
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-04 09:38:38 +0700

    fix bug

--- libs/common/src/errors/share-provider.error.ts
+++ libs/common/src/errors/share-provider.error.ts
@@ -1,16 +1,18 @@
 import { ForbiddenException, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
 
 export const ServiceProviderNotFoundException =
-    new NotFoundException([
+    new RpcException(new NotFoundException([
         { message: 'Error.ServiceProviderNotFound', path: ['id'] },
     ])
+    )
 
-export const MissingProviderIdException = new UnauthorizedException({
+export const MissingProviderIdException = new RpcException(new UnauthorizedException({
     message: 'Error.MissingProviderId',
     path: ['providerId'],
-});
+}))
 
-export const ProviderNotVerifiedException = new ForbiddenException({
+export const ProviderNotVerifiedException = new RpcException(new ForbiddenException({
     message: 'Error.ProviderNotVerified',
     path: ['providerId'],
-});
+}))
