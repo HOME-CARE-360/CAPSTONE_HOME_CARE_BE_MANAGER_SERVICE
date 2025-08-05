af245af412f9c60b8259b6721f5ed15f576bc5d8
tientran1234 <tranngoctien29112003@gmail.com>
2025-08-05 22:36:07 +0700

    fix dto

--- libs/common/src/request-response-type/with-draw/with-draw.model.ts
+++ libs/common/src/request-response-type/with-draw/with-draw.model.ts
@@ -6,18 +6,29 @@ export const GetListWidthDrawQuerySchema = z.object({
     page: z.coerce.number().int().positive().default(1),
     limit: z.coerce.number().int().positive().default(10),
     providerName: z.string().optional(),
-    status: z.enum([
-        "APPROVED", "CANCELLED", "COMPLETED", "PENDING", "REJECTED"
-    ]).array(),
+    status:
+        z
+            .union([
+                z.enum(['APPROVED', 'CANCELLED', 'COMPLETED', 'PENDING', 'REJECTED']),
+                z.array(z.enum(['APPROVED', 'CANCELLED', 'COMPLETED', 'PENDING', 'REJECTED']))
+            ])
+
+            .array(),
 
     orderBy: z.enum([OrderBy.Asc, OrderBy.Desc]).default(OrderBy.Desc),
-    sortBy: z.enum([SortByWithDraw.CreatedAt, SortByWithDraw.Amount, SortByWithDraw.ProcessedAt]).default(SortByWithDraw.CreatedAt),
-})
+    sortBy: z
+        .enum([
+            SortByWithDraw.CreatedAt,
+            SortByWithDraw.Amount,
+            SortByWithDraw.ProcessedAt,
+        ])
+        .default(SortByWithDraw.CreatedAt),
+});
 export const UpdateWithDrawalBodySchema = WithdrawalRequestSchema.pick({
     note: true,
     id: true,
     status: true,
-    
+
 })
 export type UpdateWithDrawalBodyType = z.infer<typeof UpdateWithDrawalBodySchema>
 export type GetListWidthDrawQueryType = z.infer<typeof GetListWidthDrawQuerySchema>
